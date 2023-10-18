8da6812a80521b5bdc2b51810029b38b9c3c8b1a
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2023-10-18 23:14:59 +0200

    fix: actually import the patch :facepalm:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import './vercel-broken-rewrites-patch';
 
 const inter = Inter({ subsets: ['latin'] })
 
