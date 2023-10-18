a3de765d0ef76b7b95b6e99b3b033b122be96a93
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2023-10-18 23:06:16 +0200

    feat: add demo page

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,7 @@
+export default function Demo() {
+  return (
+    <main>
+      <h1>Welcome to /demo!</h1>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import styles from './page.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Application B</h1>
+      <Link href="/demo">Go to /demo</Link>
     </main>
   )
 }
